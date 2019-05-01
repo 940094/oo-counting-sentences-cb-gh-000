38c55dbdf2d94d276a19b0cbe3496e8a7bb6709d
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 21:23:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match?(/^.*\.$/) # call .match? on itself
   end
 
   def question?
-
+    self.match?(/^.*\?$/)
   end
 
   def exclamation?
-
+    self.match?(/^.*\!$/)
   end
 
   def count_sentences
-
+    array = self.split(/[.?]/) # https://stackoverflow.com/questions/19509307/split-string-by-multiple-delimiters
+    array.length
   end
 end
