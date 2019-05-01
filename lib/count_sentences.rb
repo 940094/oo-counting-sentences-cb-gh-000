3e84a7449fd06980c42ea44dddee8a3fc9b3eae3
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 21:22:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     array = []
-    self.split(/[.?]/) # https://stackoverflow.com/questions/19509307/split-string-by-multiple-delimiters
+    array = self.split(/[.?]/) # https://stackoverflow.com/questions/19509307/split-string-by-multiple-delimiters
     array.length
   end
 end
