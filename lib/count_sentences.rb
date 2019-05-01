4bd7d5d5028a4ba42c0aa20a1a4f0e12ab3748d1
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 21:15:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match?(/^.*\.$/) # call .match? on itself
   end
 
   def question?
-
+    self.match?(/^.*\?$/)
   end
 
   def exclamation?
-
+    self.match?(/^.*\!$/)
   end
 
   def count_sentences
