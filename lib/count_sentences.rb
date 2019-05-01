746b097dc373395f977dc03f0e8e75b1ab5a957b
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 21:22:25 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,17 +20,3 @@ class String
     array.length
   end
 end
-
-
-it  "returns the number of sentences in a string" do
-  expect("one. two. three?".count_sentence).to eq(3)
-end
-
-it "returns zero if there are no sentences in a string" do
-  expect("".count_sentences).to eq(0)
-end
-
-it "returns the number of sentences in a complex string" do
-  complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
-  expect(complex_string.count_sentences).to eq(4)
-end
